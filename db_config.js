66076fac5ebac01ce10c2b8cdd85deb2d2132b77
taniania32 <37448500+taniania32@users.noreply.github.com>
2019-02-20 13:34:34 +0700

    crud

---
+++ db_config.js
@@ -0,0 +1,10 @@
+var mysql = require('mysql');
+
+var db = mysql.createConnection({
+    host: "127.0.0.1",
+    user: "root",
+    password: "",
+    database: "petani_kopi"
+});
+
+module.exports = db;
